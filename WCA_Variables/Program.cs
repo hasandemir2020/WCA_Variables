9e8155ed9559c95643148562b4aaf33eaa2db13b
Hasan DEMİR <hasan.demir@baogrenci.com>
2020-02-13 12:44:28 +0300

    Add project files.

---
+++ WCA_Variables/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+namespace WCA_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CTRL +K+D   Kodları Düzenle
+            // CTRL +E+W   
+            // CTRL +K+C   Yorum Satırına al
+            // CTRL +K+U   Yorum Satırından Al
+
+            //Tam Sayılar
+            // byte, sbyte, short, ushort, ınt, uint, long, ulong
+
+            //int 32 bit en cok kullanılan sayı
+            // uint 32 bit işaretsiz sayılar
+            // long -+Katrilyonluk Sayılar
+            /*
+            byte sayi1 = 56;
+            sbyte sayi2 = 12;
+            short sayi3 = 134;
+            ushort sayi4 = 123;
+            int sayi5 = 324;
+            uint sayi6 = 214456;
+            long sayi7 = 324234;
+            ulong sayi8 = 23424;
+            char karakter = 'A';
+            //string metin = "Bilgeadam\n Başiktaş\n Şubesi";
+            string tekKarakter = "A";
+            string tabBosluk = "Bilge Adam";
+            string metin2= "Bilge adam Besıktas \"Yazılım\" Dersleri ";
+            */
+            Console.WriteLine("Merhaba Dünya");
+            // Console.WriteLine(metin2);
+            //Console.WriteLine(sayi8);
+            //Console.WriteLine();
+            Console.ReadKey();
+
+            // \n alt satıra gecer
+            // \t tab boşluk ekler
+            // Ctrl+ D Duplikate
+
+            // Ctrl + Shift + B Build etmek
+
+
+            //Ondalıklı Sayılar
+            //Metinsel
+            //Mantıksal
+
+        }
+    }
+}
